7370fccfbf8f4e71f21fb69ff3cda78a622591cc
Divyansh Singh <sdivyansh59@gmail.com>
2022-11-08 13:09:37 +0530

    tryed to use mux but did not work

--- main.go
+++ main.go
@@ -9,10 +9,17 @@ func handlerFunc (w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, r.URL.Path)
+	
 	if(r.URL.Path == "/") {
 		fmt.Fprintf(w,"<h1> Welcome to my  awesome site! </h1>")
 	} else if (r.URL.Path == "/support") {
 		fmt.Fprintf(w,"To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com \"> support@lenselocked.com </a>.")
+	}else{
+	
+		
+		// w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w,"<h1> We did not find what you are looking for :)</h1> <br> please send us a mail.")
+		
 	}
 	
 	
@@ -21,9 +28,13 @@ func handlerFunc (w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
-	fmt.Println("Nameste Go!")
+	// fmt.Println("Nameste Go!")
+	mux := &http.ServeMux{}
+
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	// http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
+
 	/* (":3000", handler)
 	 nil means: we want default serve mux */
 
