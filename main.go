e50d79d851115b0dbb1f81cb05336973cfd25fc3
Divyansh Singh <sdivyansh59@gmail.com>
2022-11-20 21:14:26 +0530

    contact template added

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"text/template"
 
 	"github.com/gorilla/mux"
 )
 
-func handlerFunc (w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w,"To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com \"> support@lenselocked.com </a>.")
-}
+
+var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func home (w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	// fmt.Fprintf(w,"<h1> Welcome to my  awesome site! </h1>")
 	if err := homeTemplate.Execute(w,nil); err != nil {
 		panic(err)
 	}
 
-} 
+}  
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
 
-var homeTemplate *template.Template
 
 func main() {
 	var err error
@@ -31,19 +33,16 @@ func main() {
 		panic(err)
 	}
 	
+    contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
-	r.HandleFunc("/support", handlerFunc)
+	r.HandleFunc("/contact", contact)
+	
 	
 	http.ListenAndServe(":3000",r )
 }
 
-
-
-/*
-Exercise 1.
-Add an FAQ page to your application under the path /faq
-This does not need to have a lot of content, but make sure it is different from your pages.
-
-
-*/
